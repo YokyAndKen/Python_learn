f77ed2a61e1419a0852cb3c3b4382ed259b97245
Yoky <yoky599@foxmail.com>
2022-11-07 18:36:18 +0800

    1107

--- 循环语句/7.breack和continue.py
+++ 循环语句/7.breack和continue.py
@@ -1,7 +1,7 @@
-# for i in range(1, 6):
-#     print("语句1")
-#     continue
-#     print("语句2")
+for i in range(1, 6):
+    print("语句1")
+    continue
+    print("语句2")
 
 for i in range(1, 6):
     print("语句1")
@@ -10,3 +10,10 @@ for i in range(1, 6):
         continue
         print("语句3")
     print("语句4")
+
+for i in range(1, 6):
+    print("语句1111")
+    for j in range(1, 6):
+        print("语句2")
+        break
+        print("语句3")
---
+++ 循环语句/8.循环综合案例.py
@@ -0,0 +1,11 @@
+import random
+count = 10000
+for item in range(20):
+    score = int(random.randint(1, 10))
+    if score >= 5:
+        count -= 1000
+        print(f"向员工{item + 1}发放工资1000元，账户余额还剩{ count }元")
+    else:
+        print(f"员工{item + 1}，绩效分{ score }，不发工资，下一位")
+
+print
